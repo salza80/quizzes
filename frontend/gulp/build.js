b8392b65c5c4b6eb04556f0ea1e40aea8577b5c5
Sally Mclean <smclean17@gmail.com>
2016-03-05 01:18:38 +0100

    deploy to pulic assetsand fix clean

--- frontend/gulp/build.js
+++ frontend/gulp/build.js
@@ -91,8 +91,16 @@ gulp.task('other', function () {
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
 });
 
-gulp.task('clean', function () {
-  return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+// gulp.task('clean', function () {
+//   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+// });
+gulp.task('clean', function (done) {
+  $.del([path.join(conf.paths.dist, '/assets'),
+    path.join(conf.paths.dist, '/fonts'),
+    path.join(conf.paths.dist, '/maps'),
+    path.join(conf.paths.dist, '/scripts'),
+    path.join(conf.paths.dist, '/styles')
+    , path.join(conf.paths.tmp, '/')], { force: true }, done);
 });
 
 gulp.task('build', ['html', 'fonts', 'other']);
