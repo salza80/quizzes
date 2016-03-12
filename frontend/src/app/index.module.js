3e0eda218b5d4be4cfb2bb28adb45d42f43e0a8e
Sally Mclean <smclean17@gmail.com>
2016-03-12 02:07:02 +0100

    upgrade angular, remove toastr

--- frontend/src/app/index.module.js
+++ frontend/src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('frontend', ['ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'toastr']);
+    .module('frontend', ['ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute']);
 
 })();
