399e564aa179967058ce282b55e1b61ba64674b2
Sally Mclean <smclean17@gmail.com>
2016-03-14 15:16:24 +0100

    add getoutcome to quiz resource

--- frontend/src/app/components/quizzes/quiz.service.js
+++ frontend/src/app/components/quizzes/quiz.service.js
@@ -9,15 +9,19 @@
   function quiz($resource) {
     var Quiz = $resource('/api/quizzes/:name_url.json', {
       query: {method: 'GET', isArray: true, cancellable: true},
-      get: {method:'GET', params: {name_url: '@name_url'}},
-    },
-     {
-      resultcode: {
-        url: '/api/quizzes/result_code/.json',
-        method: 'POST',
-        params: {points: '@points'}
-      }
-    });
+      get: {method:'GET', params: {name_url: '@name_url'}}
+      },
+      {
+        resultcode: {
+          url: '/api/quizzes/result_code/.json',
+          method: 'POST',
+          params: {points: '@points'}
+        },
+        outcome: {
+          url: '/api/quizzes/:name_url/outcome/:result_code.json',
+          method: 'GET'
+        }
+      });
     var quizzes = {};
     quizzes.list=[];
     var quiz = {};
@@ -26,6 +30,7 @@
     this.getList = getList;
     this.getQuiz = getQuiz;
     this.getResultCode = getResultCode;
+    this.getOutcome = getOutcome;
 
     function getList() {
       quizzes.list = Quiz.query();
@@ -33,12 +38,16 @@
     }
     function getQuiz(name_url){
       quiz.active = Quiz.get({name_url: name_url});
-      return quiz.active.$promise;
+      return quiz
     }
 
     function getResultCode(points){
       quiz.result_code = Quiz.resultcode({points: points});
-      return quiz.result_code.$promise;
+      return quiz
+    }
+    function getOutcome(name_url, points){
+      quiz.outcome = quiz.outcome({name_url: name_url, points: points});
+      return quiz
     }
   }
 })();
--- frontend/src/app/components/quizzes/simpleQuiz/simpleQuiz.component.js
+++ frontend/src/app/components/quizzes/simpleQuiz/simpleQuiz.component.js
@@ -21,23 +21,21 @@
     ctrl.nextQuestion = nextQuestion;
 
     function init() {
-      quiz.getQuiz(ctrl.urlName).then(function(data){
-       quizData = data;
-       ctrl.quiz = quizData;
-       ctrl.currentQuestion = quizData.questions[currentQuestionIndex];
-       ctrl.totalQuestions = ctrl.quiz.questions.length;
-      });
+      quizData = quiz.getQuiz(ctrl.urlName)
+      quizData.active.$promise.then(function(){
+        ctrl.quiz = quizData.active
+        ctrl.currentQuestion = quizData.active.questions[currentQuestionIndex];
+        ctrl.totalQuestions = ctrl.quiz.questions.length;
+      })
     }
 
     function nextQuestion(answer){
       answers.push(answer);
-      console.log(currentQuestionIndex)
-      console.log(quizData.questions.length)
       currentQuestionIndex += 1;
-      if(currentQuestionIndex === quizData.questions.length){
-        getOutcome()
+      if(currentQuestionIndex === quizData.active.questions.length){
+        finishQuiz()
       }else {
-        ctrl.currentQuestion = quizData.questions[currentQuestionIndex];
+        ctrl.currentQuestion = quizData.active.questions[currentQuestionIndex];
       }
     } 
 
@@ -49,9 +47,12 @@
       return points;
     }
 
-    function getOutcome(){
-      quiz.getResultCode(totalPoints()).then(function(data){
-        console.log(data.result_code);
+    function finishQuiz(){
+      quiz.getResultCode(totalPoints())
+      quizData.result_code.$promise.then(function(data){
+        //redirect URL to outcome
+
+        // console.log(data.result_code);
       })
     }
   }
