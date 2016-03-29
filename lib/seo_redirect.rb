8d176d8aaf87f146abcb6d6fb82315c7383c783a
Sally Mclean <smclean17@gmail.com>
2016-03-29 15:55:00 +0200

    seo redirect detects facebook crawler and escaped_fragment, redirect not implemented

--- lib/seo_redirect.rb
+++ lib/seo_redirect.rb
@@ -1,35 +1,26 @@
-# class SeoRedirect
-#   def initialize(app)
-#     @app = app
-#   end
-  
-#   def call(env)
-#     status, headers, response = @app.call(env)
-#     [status, headers, "hello world"]
-#     if headers["Content-Type"].include? "text/html"
-#       [status, headers, "<!-- TESTING -->\n" + response.body]
-#     else
-#       [status, headers, response]
-#     end
-#   end
-# end
-
-
 class SeoRedirect
+  USER_AGENT_STRINGS = ['facebookexternalhit/1.1', 'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)', 'Facebot']
+
   def initialize(app)
     @app = app
   end
 
-  def redirect(location)
-      [301, {'Location' => location, 'Content-Type' => 'text/html'}, ['seo version']]
-  end
+  # def redirect(location)
+  #     [301, {'Location' => location, 'Content-Type' => 'text/html'}, ['seo version']]
+  # end
 
   def call(env)
     request = Rack::Request.new(env)
-    query_hash = Rack::Utils.parse_query(request.path) 
-    puts query_hash
-    if query_hash.has_key?('_escaped_fragment_')
-        puts 'REDIRECT TO SEO STATIC PAGE'
+    query_hash = Rack::Utils.parse_query(request.path)
+    puts "query_hash: #{query_hash}"
+    puts "user agent: #{request.user_agent}"
+    puts "params: #{request.params}"
+
+    # puts query_hash
+    if USER_AGENT_STRINGS.include?(request.user_agent) || request.params.has_key?('_escaped_fragment_')
+      # should ignore google crawler
+        puts 'WILL REDIRECT TO SEO STATIC PAGE'
+        @app.call(env)
     else
       # return redirect(redirects[req.path]) if redirects.include?(req.path)
       @app.call(env)
